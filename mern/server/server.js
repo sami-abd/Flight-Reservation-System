f5ad6667ede2b0db4dd4036189abba920bd35d3f
Brandon Lac <brandon.lac@ucalgary.ca>
2023-11-30 20:47:01 -0700

    second commit

--- mern/server/server.js
+++ mern/server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
@@ -12,6 +12,6 @@ app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
-   });
+  });
   console.log(`Server is running on port: ${port}`);
 });
